99deac923d440dcabd2c3911cd6f7e9bc423a820
debbiechiang <sangoire@gmail.com>
2015-06-17 15:53:32 -0400

    Protoyping in SASS to get a feel for the interaction.

---
+++ public/js/all.js
@@ -0,0 +1,8 @@
+// temporary helper functions 
+function addClass(sel, n, className){
+	document.getElementsByClassName(sel)[n].classList.add(className);
+}
+
+function removeClass(sel, n, className){
+	document.getElementsByClassName(sel)[n].classList.remove(className);
+}
